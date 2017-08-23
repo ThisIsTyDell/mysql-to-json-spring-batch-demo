1de7f1f1f24572e4304b556cca148dec06fad1c2
TyDell Miller <tymiller@LNAR-PB00V9X1.corp.capgemini.com>
2017-08-23 10:55:37 -0500

    moved database conn to customeritemreader

--- src/main/java/com/capgemini/dbtojson/JobCompletionNotificationListener.java
+++ src/main/java/com/capgemini/dbtojson/JobCompletionNotificationListener.java
@@ -2,36 +2,21 @@ package com.capgemini.dbtojson;
 
 import java.util.List;
 
-//import javax.sql.DataSource;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
-//import org.springframework.batch.item.ExecutionContext;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.jdbc.core.BeanPropertyRowMapper;
-//import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JobCompletionNotificationListener implements JobExecutionListener {
 	
-	//@Autowired
-	//public DataSource dataSource;
-	
 	private static Logger log = LogManager.getLogger();
 	
     @Override
     public void beforeJob(JobExecution jobExecution) {
-    	//JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-    	//List<Customer> customerList = jdbcTemplate.query("SELECT * FROM customers",
-    	//		new BeanPropertyRowMapper<Customer>(Customer.class));
-   
-    	//ExecutionContext jobContext = jobExecution.getExecutionContext();
-    	//jobContext.put("customerList", customerList);
-    	
     	log.info("!!! JOB STARTED!");
     }
 	
